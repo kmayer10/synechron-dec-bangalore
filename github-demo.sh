d4ff30bc2e5c4c5c1e92e3cdd8862c6796e96be6
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2019-12-17 13:40:14 +0530

    KT-10 Diff between pull & fetch

--- github-demo.sh
+++ github-demo.sh
@@ -2,3 +2,4 @@
 
 echo "This is GITHUB Demo file"
 echo "This is second change to show difference between remote and local branch"
+echo "This is third change to show difference between PULL and FETCH"
